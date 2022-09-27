7449606d1198b4263726e9fca0eb27aa032ec5bf
Artemson <60414364+AldnoahZero-00@users.noreply.github.com>
2022-09-28 01:03:59 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
         $table->id();
-        $table->string('name');
-        $table->string('phone');
-        $table->string('gender');
-        $table->string('dob');
-        $table->string('user_type')->nullable(); 
+        $table->string('lastname');
+        $table->string('firstname');
+        $table->string('middlename');
+        $table->string('student_type')->nullable();
+        $table->string('old_id')->nullable(); 
         $table->string('email')->unique();
         $table->timestamp('email_verified_at')->nullable();
         $table->string('password');
